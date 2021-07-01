5f7a335367029dbd820ad4e349cb81fa071d4873
pipe2442 <felipeangulo2442@gmail.com>
2021-06-30 22:58:38 -0500

    Add testing for capitalize - reverse string - and calculator

---
+++ src/calculator.js
@@ -0,0 +1,16 @@
+const calculator = {
+    sum: (a, b) => {
+        return a + b;
+    },
+    subs: (a, b) => {
+        return a - b;
+    },
+    multiply: (a, b) => {
+        return a * b;
+    },
+    divide: (a, b) => {
+        return a / b;
+    }
+}
+
+module.exports = calculator;
---
+++ test/calculator.test.js
@@ -0,0 +1,19 @@
+const calculator = require('../src/calculator')
+
+describe("calculator functions", () => {
+    test('expect sum 2 + 3 to be 5', () => {
+        expect(calculator.sum(2, 3)).toBe(5);
+    });
+
+    test('expect substraction 5 - 3 to be 2', () => {
+        expect(calculator.subs(5, 3)).toBe(2);
+    });
+    
+    test('expect multiplication 5 * 5 to be 25', () => {
+        expect(calculator.multiply(5, 5)).toBe(25);
+    });
+
+    test('expect divition 25 / 5 to be 5', () => {
+        expect(calculator.divide(25, 5)).toBe(5);
+    });
+})
